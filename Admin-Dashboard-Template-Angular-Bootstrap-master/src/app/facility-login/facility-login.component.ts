eb38064d13f06e2280dc2d88a4fc90f221cb803f
Akhilesh Pothuri <pothuriakhilesh@yahoo.com>
2019-10-21 01:54:08 +0530

    Login Page

--- Admin-Dashboard-Template-Angular-Bootstrap-master/src/app/facility-login/facility-login.component.ts
+++ Admin-Dashboard-Template-Angular-Bootstrap-master/src/app/facility-login/facility-login.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-
 @Component({
-  selector: 'app-facility-login',
+  selector: 'app-root',
   templateUrl: './facility-login.component.html',
   styleUrls: ['./facility-login.component.scss']
 })
-export class FacilityLoginComponent implements OnInit {
-
+export class FacilityLoginComponent {
+  @ViewChild('f', { static: false }) signupForm: NgForm;
   constructor(
     private router: Router
   ) { }
-
-  ngOnInit() {
-  }
-
+  user = {
+  
+    email: '',
+  
+  };
+  submitted = false;
   verifyCred(){
     console.log("Going from facility to login");
     this.router.navigateByUrl('/login/olduser');
   }
+  
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
 
-
+  
 }

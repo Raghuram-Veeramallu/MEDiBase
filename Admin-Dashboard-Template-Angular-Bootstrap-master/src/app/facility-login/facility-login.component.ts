6fc7c4c01dea6bb583601172f2306cc76345a26e
Raghuram Veeramallu <rockstar@dynamo.local>
2019-11-05 00:01:14 +0530

    login_info_retrieved

--- Admin-Dashboard-Template-Angular-Bootstrap-master/src/app/facility-login/facility-login.component.ts
+++ Admin-Dashboard-Template-Angular-Bootstrap-master/src/app/facility-login/facility-login.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { RecordsService } from '../shared/services/records.service';
+//import { RecordsService } from '../shared/services/records.service';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +12,21 @@ export class FacilityLoginComponent {
   @ViewChild('f', { static: false }) signupForm: NgForm;
   constructor(
     private router: Router,
-    private records: RecordsService
+    //private records: RecordsService
   ) {
-    this.records.run();
+    //this.records.run();
   }
-  user = {
-  
-    email: '',
-  
+  institute = {
+    facilityId: '',
+    facilityPasskey: ''
   };
   submitted = false;
-  verifyCred(){
+
+  instituteLogin(instForm: NgForm){
     console.log("Going from facility to login");
+    //DONE:   instForm.value['facilityID']
+    // instForm.value['facilityPasskry']
+    console.log(instForm.value['facilityId'].toString());
     this.router.navigateByUrl('/login/olduser');
   }
   newUser(){
--- Admin-Dashboard-Template-Angular-Bootstrap-master/src/app/shared/services/records.service.ts
+++ Admin-Dashboard-Template-Angular-Bootstrap-master/src/app/shared/services/records.service.ts
@@ -1,6 +1,6 @@
 
 import { Injectable } from '@angular/core';
-import { Client } from 'cassandra-driver';
+//import { Client } from 'cassandra-driver';
 
 
 @Injectable()
@@ -9,18 +9,18 @@ export class RecordsService {
     constructor(){
     }
 
-    async run(){
-        const client = new Client({
-            contactPoints: ['127.0.0.1'],
-            localDataCenter: 'datacenter1'
-        });
+    // async run(){
+    //     const client = new Client({
+    //         contactPoints: ['127.0.0.1'],
+    //         localDataCenter: 'datacenter1'
+    //     });
 
-        await client.connect();
+    //     await client.connect();
 
-        const rs = await client.execute('SELECT * FROM system.local');
-        const row = rs.first();
-        console.log(`Connected to cluster: ${row['cluster_name']}`);
+    //     const rs = await client.execute('SELECT * FROM system.local');
+    //     const row = rs.first();
+    //     console.log(`Connected to cluster: ${row['cluster_name']}`);
 
-        await client.shutdown();
-    }
+    //     await client.shutdown();
+    // }
 }

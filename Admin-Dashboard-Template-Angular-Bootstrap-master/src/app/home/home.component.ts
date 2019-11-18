93d66dc742f98bd031e0cc26538196bfb99759de
Raghuram Veeramallu <rockstar@dynamo.local>
2019-11-18 22:33:33 +0530

    shit

--- Admin-Dashboard-Template-Angular-Bootstrap-master/src/app/home/home.component.ts
+++ Admin-Dashboard-Template-Angular-Bootstrap-master/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+//import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -30,16 +30,15 @@ export class HomeComponent implements OnInit {
   constructor(
     private router: Router,
     private location: Location,
-    private httpClient: HttpClient
+    //private httpClient: HttpClient
     ) {
       this.router.events.subscribe((route:any) => {
         this.currentUrl = route.url;
         this.specialPage = this.specialPages.indexOf(this.currentUrl) !== -1;
       });
-      this.getAllYears("rTHDf5bLW0SjpMAndIAOxQEXxgB3").then( _data => {
-        console.log(this.years[0].all_years);
-        }
-      )
+      
+      //  "rTHDf5bLW0SjpMAndIAOxQEXxgB3"
+
     }
 
   ngOnInit() {
@@ -47,18 +46,6 @@ export class HomeComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
-  }
-
-  getAllYears(patientid:string){
-
-    var posting = {
-        patientUID: patientid
-    }
-
-    return this.httpClient.post("http://localhost:3000/api/allYears/", posting).toPromise().then((data) => {
-        console.log(data as JSON);
-        this.years = data;
-    })
   }
 
 }
